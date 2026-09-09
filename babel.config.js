e198e27038073a7677735406d1e0c1563f763827
gngie0472-ux <gngie0472@gmail.com>
2026-09-09 16:30:25 +0100

    Update babel.config.js to remove a plugin
    Removed the transform-inline-environment-variables plugin to avoid build errors.

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,8 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      // تم حذف إضافة transform-inline-environment-variables لتجنب خطأ البناء
       // يجب أن يظل plugin الخاص بـ reanimated في نهاية القائمة دائماً
       'react-native-reanimated/plugin',
     ],
