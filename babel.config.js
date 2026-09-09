550fb367890e5c7f6e824949b7def85b3ab52c28
gngie0472-ux <gngie0472@gmail.com>
2026-09-09 15:34:13 +0100

    Update babel.config.js to include additional plugins
    Added plugins for inline environment variables and Reanimated.

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      // دعم الـ Inline Environment Variables لـ Expo
+      'transform-inline-environment-variables',
+      // إضافة Reanimated plugin في النهاية دائماً إذا كنت تستخدمه في المشروع
+      'react-native-reanimated/plugin',
+    ],
   };
 };
