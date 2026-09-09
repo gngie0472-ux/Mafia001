debc5942b7429f677dc59955924037a525eb6b8e
gngie0472-ux <gngie0472@gmail.com>
2026-09-09 16:49:28 +0100

    Remove react-native-reanimated plugin from config
    Removed the react-native-reanimated plugin from the Babel configuration.

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,5 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      // يجب أن يظل plugin الخاص بـ reanimated في نهاية القائمة دائماً
-      'react-native-reanimated/plugin',
-    ],
   };
 };
